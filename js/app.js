cc3ed4cb2975113b151fce3b2eb814443ab3169a
Emilse Arias <emilsearias@gmail.com>
2023-12-04 17:22:00 -0300

    podemos crear un contacto desde app.js

---
+++ js/app.js
@@ -0,0 +1,5 @@
+import Contacto from "./classContacto.js";
+
+const nuevoContacto = new Contacto('juan', 'perez', 'juan@gmail.com', 234234);
+console.log(nuevoContacto)
+
---
+++ js/classContacto.js
@@ -0,0 +1,51 @@
+export default class Contacto {
+  #id;
+  #nombre;
+  #apellido;
+  #email;
+  #telefono;
+
+  constructor(nombre, apellido, email, telefono) {
+    this.#id = crypto.randomUUID() ;
+    this.#nombre = nombre;
+    this.#apellido = apellido;
+    this.#email = email;
+    this.#telefono = telefono;
+  }
+
+  get id() {
+    return this.#id;
+  }
+
+  get nombre() {
+    return this.#nombre;
+  }
+
+  set nombre(nuevoNombre) {
+    this.#nombre = nuevoNombre;
+  }
+
+  get apellido() {
+    return this.#apellido;
+  }
+
+  set apellido(nuevoApellido) {
+    this.#apellido = nuevoApellido;
+  }
+
+  get email() {
+    return this.#email;
+  }
+
+  set email(nuevoEmail) {
+    this.#email = nuevoEmail;
+  }
+
+  get telefono() {
+    return this.#telefono;
+  }
+
+  set telefono(nuevoTelefono) {
+    this.#telefono = nuevoTelefono;
+  }
+}
