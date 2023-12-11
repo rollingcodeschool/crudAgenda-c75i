ca8eff3a0a996d48f33b9f4cec85ad6b20c9148a
Emilse Arias <emilsearias@gmail.com>
2023-12-11 17:23:33 -0300

    agregamos una funcion para validar cantidad de caracteres de un input

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ const nombre = document.querySelector("#nombre"),
   apellido = document.querySelector("#apellido"),
   email = document.querySelector("#email"),
   telefono = document.querySelector("#telefono");
-  const agenda = JSON.parse(localStorage.getItem('agendaKey')) || [];
+const agenda = JSON.parse(localStorage.getItem("agendaKey")) || [];
 
 //funciones
 const crearContacto = (e) => {
@@ -15,28 +15,37 @@ const crearContacto = (e) => {
   //en el evento submit
   //preventDefault
   //tomo los datos de los inputs (validar)
-  //crear un objeto
-    const nuevoContacto = new Contacto(nombre.value, apellido.value, email.value, telefono.value);
-  //guardo el objeto en un array
-   agenda.push(nuevoContacto)
-   console.log(agenda);
-   limpiarFormularioContacto();
-  //guardar el array en el localstorage JSON
-  guardarEnLocalstorage();
-  //dibujar una fila en la tabla
-  dibujarFila(nuevoContacto, agenda.length);
+  if (validarCantidadCaracteres(nombre.value, 3, 25) && validarCantidadCaracteres(apellido.value, 2, 35)) {
+    //crear un objeto
+    const nuevoContacto = new Contacto(
+      nombre.value,
+      apellido.value,
+      email.value,
+      telefono.value
+    );
+    //guardo el objeto en un array
+    agenda.push(nuevoContacto);
+    console.log(agenda);
+    limpiarFormularioContacto();
+    //guardar el array en el localstorage JSON
+    guardarEnLocalstorage();
+    //dibujar una fila en la tabla
+    dibujarFila(nuevoContacto, agenda.length);
+  }else{
+    alert('hay errores en los datos del formulario')
+  }
 };
 
-const limpiarFormularioContacto = ()=>{
-    formularioContacto.reset();
-}
+const limpiarFormularioContacto = () => {
+  formularioContacto.reset();
+};
 
-const guardarEnLocalstorage = ()=>{
-    localStorage.setItem('agendaKey', JSON.stringify(agenda));
-}
+const guardarEnLocalstorage = () => {
+  localStorage.setItem("agendaKey", JSON.stringify(agenda));
+};
 
-const dibujarFila = (contacto, numeroFila)=>{
-  const tablaContactos = document.getElementById('tablaContacto');
+const dibujarFila = (contacto, numeroFila) => {
+  const tablaContactos = document.getElementById("tablaContacto");
   tablaContactos.innerHTML += `<tr>
   <th scope="row">${numeroFila}</th>
   <td>${contacto.nombre}</td>
@@ -48,35 +57,50 @@ const dibujarFila = (contacto, numeroFila)=>{
     <button class="btn btn-warning">Editar</button>
     <button class="btn btn-danger" onclick="borrarContacto('${contacto.id}')">Borrar</button>
   </td>
-</tr>`
-}
+</tr>`;
+};
 
-const cargaInicial = () =>{
+const cargaInicial = () => {
   //preguntar si la agenda tiene elementos
-  if(agenda.length > 0){
-    agenda.map((itemContacto, posicionContacto)=> dibujarFila(itemContacto, posicionContacto + 1))
+  if (agenda.length > 0) {
+    agenda.map((itemContacto, posicionContacto) =>
+      dibujarFila(itemContacto, posicionContacto + 1)
+    );
   }
-}
+};
 
-window.borrarContacto =(idContacto)=>{
+window.borrarContacto = (idContacto) => {
   console.log(idContacto);
- //buscar la posicion del elemento en el array findIndex
- const posicionContactoBuscado = agenda.findIndex((itemContacto) => itemContacto.id === idContacto );
- console.log(posicionContactoBuscado);
+  //buscar la posicion del elemento en el array findIndex
+  const posicionContactoBuscado = agenda.findIndex(
+    (itemContacto) => itemContacto.id === idContacto
+  );
+  console.log(posicionContactoBuscado);
   //borrar el contacto de la agenda usando splice(posicion, cant de elementos a borrar)
-  agenda.splice(posicionContactoBuscado, 1)
+  agenda.splice(posicionContactoBuscado, 1);
   //actualizar localstorage
-  guardarEnLocalstorage()
+  guardarEnLocalstorage();
   //borrar la fila de la tabla
-  const tablaContactos = document.getElementById('tablaContacto');
-  tablaContactos.innerHTML = '';
+  const tablaContactos = document.getElementById("tablaContacto");
+  tablaContactos.innerHTML = "";
   cargaInicial();
-}
+};
 
-window.detalleContacto = (idContacto)=>{
-  console.log(window.location)
-  window.location.href = window.location.origin +'/pages/detalleContacto.html?id='+idContacto;
-}
+window.detalleContacto = (idContacto) => {
+  console.log(window.location);
+  window.location.href =
+    window.location.origin + "/pages/detalleContacto.html?id=" + idContacto;
+};
+
+const validarCantidadCaracteres = (texto, min, max) => {
+  if (texto.length >= min && texto.length <= max) {
+    // inputNombre.className = 'form-control is-valid'
+    return true;
+  } else {
+    // inputNombre.className = 'form-control is-valid'
+    return false;
+  }
+};
 
 //resto de la logica
 formularioContacto.addEventListener("submit", crearContacto);
